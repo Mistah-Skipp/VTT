6040d93f1fb6558ccb34f51eed7a5464d925e6ca
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2022-11-21 18:10:31 -0500

    started building up framework -css: has coloring for visual aid -started work on right side -canvas is in but not interactable

---
+++ public/script/sideCol.js
@@ -0,0 +1,13 @@
+console.log("sideCol.js loaded");
+
+$("#sideCol").on('click', '#chatButton', function () {
+    console.log("chat");
+});//chat
+
+$("#sideCol").on('click', '#charButton', function () {
+    console.log("char");
+});//char
+
+$("#sideCol").on('click', '#diceButton', function () {
+    console.log("dice");
+});//dice
