86d5b798c113633924d0095d7bb2288e20f53449
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2022-11-22 13:45:49 -0500

    proof of concept for canvas + easelJS is functional

--- public/script/drawOnCanvas.js
+++ public/script/drawOnCanvas.js
@@ -1,83 +1,53 @@
+var canvas, stage, offset, mouseTarget, dragStart;
+var update = true;
 
-function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);
+function init() {
+  canvas = document.getElementById("test");
+  stage = new createjs.Stage(canvas);
+
+  //mouse enable
+  stage.enableMouseOver(1);
+
+  handleLoader();
 }
 
-var myGameArea = {
-    canvas : document.createElement('canvas'),
-    start : function() {
-        this.canvas.width = 1920;
-        this.canvas.height = 1080;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[2])
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('mousemove', function (e) {
-            myGameArea.x = e.pageX - 20;
-            myGameArea.y = e.pageY - 200;
-          })
-    },
-    clear : function(){
-        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
-    }
+function handleLoader(event){
+  //var object = event.target;
+  var circle;
+  //var drawArea = new createjs.Container();
+  
+  $("#mainArea").on('click', '#spawn', function () {
+    circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(10,10,40);
+    circle.x = (Math.random() * 1920) + 50;
+    circle.y = (Math.random() * 1080)+ 50;
+    
+    stage.addChild(circle);
+    stage.update();
+    
+    circle.on("mousedown", function (evt){
+      this.parent.addChild(this);
+      this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
+    });
+    
+    circle.on("pressmove", function (ext){
+      this.x = ext.stageX + this.offset.x;
+      this.y = ext.stageY + this.offset.y;
+      update = true;
+      
+    });
+    
+  });
+  createjs.Ticker.addEventListener(("tick"), tick);
 }
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-      }
+
+function tick(ticked){
+  if(update){
+    update = false;
+    stage.update(ticked);
   }
-
-  function updateGameArea() {
-    var timer;
-    window.addEventListener("mousedown", function (){
-         timer = this.setInterval(function(){
-            myGameArea.clear();
-            if (myGameArea.x && myGameArea.y) {
-                myGamePiece.x = myGameArea.x;
-                myGamePiece.y = myGameArea.y;
-              }
-            myGamePiece.update();
-        }, 10);
-    });
-    window.addEventListener("mouseup", function(){
-        if(timer) clearInterval(timer)
-    });
-
-  }
+}
 
 
+  //https://www.createjs.com/demos/easeljs/draganddrop
 
-
-
-  function moveup() {
-    myGamePiece.speedY -= 1;
-  }
-  
-  function movedown() {
-    myGamePiece.speedY += 1;
-  }
-  
-  function moveleft() {
-    myGamePiece.speedX -= 1;
-  }
-  
-  function moveright() {
-    myGamePiece.speedX += 1;
-  }
-
-  function stopMove() {
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-  }
