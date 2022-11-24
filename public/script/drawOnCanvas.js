4a7b9e5e225a5e633b5754aea9949dde8baa2050
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2022-11-24 00:08:34 -0500

    added square grid and option to toggle it on/off

--- public/script/drawOnCanvas.js
+++ public/script/drawOnCanvas.js
@@ -1,19 +1,72 @@
-var stage, layer;
+var stage, layer, gridLayer;
+var gridButtonStatus = 1;
 
 function init(){
   stage = new Konva.Stage({
     container: "gameArea",
-    height: 1000,
+    height: 1920,
     width:1440
     });
-   layer = new Konva.Layer();
+    layer = new Konva.Layer();
+    drawGrid();
+  }//EOF init()
+
+  function drawGrid() {
+    const gridSize = 72;
+    gridLayer = new Konva.Layer();
+
+    const xSize = stage.width(),
+          ySize = stage.height(),
+          xSteps = (Math.round(xSize/gridSize)),
+          ySteps = (Math.round(ySize/gridSize));
+     // draw vertical lines
+  for (let i = 0; i <= xSteps; i++) {
+    gridLayer.add(
+      new Konva.Line({
+        x: i * gridSize,
+        points: [0, 0, 0, ySize],
+        stroke: 'black',
+        strokeWidth: 1,
+      })
+    );
+  }
+  //draw Horizontal lines
+  for (let i = 0; i <= ySteps; i++) {
+    gridLayer.add(
+      new Konva.Line({
+        y: i * gridSize,
+        points: [0, 0, xSize, 0],
+        stroke: 'black',
+        strokeWidth: 1,
+      })
+    );
+  }
+  gridLayer.batchDraw();
+  stage.add(gridLayer);
+
+}
+//TOGGLE GRID
+$("body").on('click','#gridToggle', function(){
+  if(gridButtonStatus == 1){
+    console.log("on");
+    gridLayer.opacity(0);
+    gridButtonStatus = 0;
+  } else {
+    console.log("off");
+    gridLayer.opacity(1);
+    gridButtonStatus = 1;
+  }
+
+});//EOF TOGGLE GRID
 
 
+
+//TEST TOKEN
   $("body").on('click',"#spawn", function(){
     var token = new Konva.Circle({
         x: Math.random() *(stage.width()),
         y: Math.random() *(stage.height()),
-        radius: 48,
+        radius: 36,
         fill: 'cyan',
         draggable: true,
       });   
@@ -32,9 +85,8 @@ function init(){
             token.y(stage.height()-token.radius());
         }
       })
-    });//onclick EOF
-    
-  }
+    });//EOF on click
+//EOF TEST TOKEN
 
 
   //96px = inch
