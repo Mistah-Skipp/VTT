8ebf44505380358a8549a86518bc2bf45aa331d7
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2022-11-30 00:05:00 -0500

    able to drag and drop very crude dice onto canvas -need to make them always appear over tokens -need to rewrite how they are drawn, specific draws rather than a general w/ sizing prior -add labels with rand num gen for them -add bouncing/movement when placed -have them spawn from mouse

--- public/script/drawOnCanvas.js
+++ public/script/drawOnCanvas.js
@@ -1,16 +1,47 @@
-var stage, layer, gridLayer;
+
+var stage , layer , gridLayer;
 var gridButtonStatus = 0;
 const gridSize = 72;
 var testID = 0;
+const numDice = [];
+var diceSize = 0, diceAdder;
 
 //INIT()
+window.onload = function(){
   stage = new Konva.Stage({
     container: "gameArea",
     height: 1920,
     width:1440
-    });
+  });
   layer = new Konva.Layer();
   drawGrid();
+  
+  
+  var diceBox = document.getElementById('dice-holder-container').childNodes;
+  //adds the following eventListeners for each type of dice
+  for (let i = 0; i< diceBox.length-1; i++){
+    diceBox[i].addEventListener("dragstart", function(evt){
+      diceAdder = stage.container();
+      diceSize = this.id;
+      numDice.push(diceSize);
+      
+    diceAdder.addEventListener('dragover', function(evt){
+        evt.preventDefault();   
+        diceAdder.addEventListener('drop', function(){
+          if(diceSize != 0){
+            for(let i = 0 ; i < numDice.length; i ++){
+              drawDice(numDice[i]);
+            }
+            console.log("------")
+          }
+          //reset stored diceSize & array to avoid printing out dice X times dragover was active
+          diceSize = 0; 
+          numDice.length = 0;
+        });//diceAdder.drop
+      });//diceAdder.dragover
+    });//diceBox[i].dragstart
+  }//for loop
+}
 //EOF init()
 
 function drawGrid() {
@@ -62,30 +93,6 @@ $("body").on('click','#gridToggle', function(){
 
 });//EOF TOGGLE GRID
 
-//DICE DRAG
-      // what is url of dragging element?
-      var itemURL = '';
-      document.getElementById('dice-holder-container').addEventListener('dragstart', function (e) {
-          itemURL = e.target.src;
-        });
-
-      var con = stage.container();
-      con.addEventListener('dragover', function (e) {
-        e.preventDefault(); // !important
-      });
-
-      con.addEventListener('drop', function (e) {
-        e.preventDefault();
-        stage.setPointersPositions(e);
-        var imgGet = document.getElementById('dice-holder-container').childNodes;
-        console.log(imgGet);                              //DEBUG
-        for(let i = 0; i <= imgGet.length -1; i++){
-          if(imgGet[i].id != null){
-            console.log("id: " + imgGet[i].id);            //DEBUG
-          }
-        }
-      });
-//DICE DRAG EOF
 
 /*
 -----------------------------------------------------------------------------
@@ -98,7 +105,69 @@ curr dice plan
 -----------------------------------------------------------------------------
 */
 
+//DRAW DICE
+function drawDice(size){
+  if(size == 4){
+    size -= 1;
+  } else if (size == 8 || size == 6){
+    size = 4;
+  } else if (size == 20){
+    size = 6;
+  }
+  var newDice = new Konva.RegularPolygon({
+    x: Math.random()* 1000,
+    y: Math.random()* 500,
+    sides: size,
+    fill: "white",
+    radius: 36,
+    id: "dice",
+    draggable: true
+  });
+  layer.add(newDice);
+  stage.add(layer);
+  newDice.on('dblclick', function(){
+    newDice.destroy();
+  });
 
+/*
+-------------------------------------------------
+draw dice based off size recieved
+use konva polygons
+possibly dont need a switch case, just one general constructor
+have text attach to it that display a random num from [1,size]
+on click delete dice? or delete after x secs?
+
+might  need a slightly special switch case
+d4 - side: 3
+d6 & d8 side: 4, rotate d8 45degree before drawing
+d20 side:6
+
+
+-------------------------------------------------
+*/
+  switch(size){
+    case "4":
+      console.log("D4");
+      break;
+    case "6":
+      console.log("D6");
+      break;
+    case "8":
+      console.log("D8");
+      break;
+    case "10":
+      console.log("D10");
+      break;
+    case "12":
+      console.log("D12");
+      break;
+    case "20":
+      console.log("D20");
+      break;
+  }
+}
+
+//EOF DRAW DICE
 
 //TEST TOKEN                                                                   //DEBUG
 $("body").on('click',"#spawn", function(){
@@ -127,3 +196,41 @@ $("body").on('click',"#spawn", function(){
     })
   });//EOF on click
 //EOF TEST TOKEN
+
+
+//TEST IMG UPLOAD
+$("#file_input").change(function(e){
+
+  var URL = window.webkitURL || window.URL;
+  var url = URL.createObjectURL(e.target.files[0]);
+  var img = new Image();
+  img.src = url;
+
+
+  img.onload = function() {
+
+    var img_width = img.width;
+    var img_height = img.height;
+
+    // calculate dimensions to get max 300px
+    var max = 300;
+    var ratio = (img_width > img_height ? (img_width / max) : (img_height / max))
+
+    // now load the Konva image
+    var theImg = new Konva.Image({
+      image: img,
+      x: 50,
+      y: 30,
+      width: img_width/ratio,
+      height: img_height/ratio,
+      draggable: true
+    });
+
+    layer.add(theImg);
+    stage.add(layer);
+    layer.draw();
+  }
+
+
+});
+//EOF TEST IMG UPLOAD
